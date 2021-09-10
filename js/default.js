d98d90112db5d134a0c4f77f30c906e79e7fc9aa
Sentinm <gahm.scema@gmail.com>
2021-09-10 14:23:19 +0530

    Textarea is fit to window by Codemirror

--- js/default.js
+++ js/default.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	//code here...
-	var code = $(".codemirror-textarea")[900];
+	var code = $(".codemirror-textarea")[0];
 	var editor = CodeMirror.fromTextArea(code, {
 		lineNumbers : true
-	});
+	}).setSize("100%", 609.4);
 });
 

c0e261a8eb6dcdf86f0d97aa22b8633e4e154aa8
Sentinm <gahm.scema@gmail.com>
2021-09-10 13:24:59 +0530

    Trying...

--- js/default.js
+++ js/default.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	//code here...
-	var code = $(".codemirror-textarea")[0];
+	var code = $(".codemirror-textarea")[10];
 	var editor = CodeMirror.fromTextArea(code, {
 		lineNumbers : true
 	});
